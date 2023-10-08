0d4b9a2a89dba28902bc448c7c258f8982a75ec2
eric-blake <>
2023-10-08 16:14:07 +0000

    Update settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = 'django-insecure-&mxds&_oxb954yi$50^-bd$9img8gxdf!6w)3myw@&!6ywtgng
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-eric-blake-fsf-hello-dja-lkuj2lbe1d.us2.codeanyapp.com',
-                 '8002-eric-blake-fsf-hello-dja-lkuj2lbe1d.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['eb-django-todo-app-0c0b5b0a7d69.herokuapp.com']
 
 
 # Application definition
